2deb69eecc32db256573e4e72f9e436d3bf1b64f
Noahpetersen <noahpetersen68@outlook.de>
2024-05-16 11:01:27 +0200

    Minigame Counting Logic

--- SS24_ExamGame/Assets/Scripts/GameManager.cs
+++ SS24_ExamGame/Assets/Scripts/GameManager.cs
@@ -1,18 +1,42 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance; 
+
+    [Header("Minigame")]
+    private List<GameObject> grabbedObjectsList = new List<GameObject>();
+
+    [Header("UI")]
+    public TextMeshPro counter;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ObjectGrabbed (GameObject grabbedObject)
     {
-        
+        if(grabbedObjectsList.Contains(grabbedObject))
+        {
+            return;
+        }
+
+        grabbedObjectsList.Add(grabbedObject);
+        counter.text = grabbedObjectsList.Count.ToString();
     }
 }
+
