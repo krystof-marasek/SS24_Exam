8f740dfd1742d01fb65d00218026eba9fb2210d9
Noahpetersen <noahpetersen68@outlook.de>
2024-05-23 15:23:17 +0200

    Random Affirmation sounds

--- SS24_ExamGame/Assets/Scripts/GameManager.cs
+++ SS24_ExamGame/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -41,6 +42,18 @@ public class GameManager : MonoBehaviour
             grabbedObjectsList.Add(grabbedObject);
             StartCoroutine(joyMeter.IncreaseJoy());
             cat.grabbedRightObject = true;
+
+            if(Random.Range(0f, 1f) > 0.5f) {
+                AudioManager.instance.PlayAffirmation(AudioManager.instance.correctAffirmationSounds[Random.Range(0, AudioManager.instance.correctAffirmationSounds.Length)].name);
+            }
+            
+        } 
+        else 
+        {
+            if(Random.Range(0f, 1f) > 0.5f) {
+                AudioManager.instance.PlayAffirmation(AudioManager.instance.wrongAffirmationSounds[Random.Range(0, AudioManager.instance.wrongAffirmationSounds.Length)].name);
+            }
+            
         }
 
         if(grabbedObjectsList.Count == 6)
