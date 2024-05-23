4183c1ceb21c69f696194ff80ac87c954582f7fa
Noahpetersen <noahpetersen68@outlook.de>
2024-05-23 11:33:25 +0200

    Joy Meter Logic

---
+++ SS24_ExamGame/Assets/Scripts/JoyMeter.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoyMeter : MonoBehaviour
+{
+    [SerializeField] private GameObject progress;
+    private Coroutine increaseJoyCoroutine;
+    [SerializeField] private float animationDuration = 1f;
+    [SerializeField] private int targetItems = 6;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(100 / targetItems / 100);
+            if (increaseJoyCoroutine != null)
+            {
+                StopCoroutine(increaseJoyCoroutine);
+            }
+            increaseJoyCoroutine = StartCoroutine(IncreaseJoy());
+        }
+    }
+
+    private IEnumerator IncreaseJoy()
+    {
+        Image joy = progress.GetComponent<Image>();
+        float increment = (100f / targetItems) / 100f;
+        float targetFill = Mathf.Clamp(joy.fillAmount + increment, 0, 1);
+        float startFill = joy.fillAmount; 
+        float elapsed = 0f;
+
+        while (elapsed < animationDuration)
+        {
+            elapsed += Time.deltaTime;
+            joy.fillAmount = Mathf.Lerp(startFill, targetFill, elapsed / animationDuration);
+            yield return null;
+        }
+        joy.fillAmount = targetFill;
+    }
+}
